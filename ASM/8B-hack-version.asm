c4570a848c1cab55cc438736238c1558c01bdaac
alex-west <rt55j1@gmail.com>
2026-05-09 23:45:02 -0700

    Fix incorrect "Ver." attribute on title

--- ASM/8B-hack-version.asm
+++ ASM/8B-hack-version.asm
@@ -3,6 +3,15 @@
 org $8B8697
     NOP
 
+org $8B86E0 ; Skips drawing the game header version number and mute flag.
+    bra skipThing
+    
+org $8B873D
+skipThing:
+
+org $8B877A ; 'Ver.' OAM entry tile numbers and attributes
+    dw $39F2, $39F1, $39F0
+
 ;;;; $F754: Debug. Version string ;;;
 ; '02.02.21.30'
 ;$8B:F754             db 30, 32, 2E, 30, 32, 2E, 32, 31, 2E, 33, 30, 00
